a4e5b97077ad549bcd976163b3ef10be53928af0
diogodmsena <diogodmsena@gmail.com>
2026-04-11 23:07:05 -0300

    fix: restaurar babel.config.js correto e inicializar reanimated no _layout.tsx

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Stack } from 'expo-router';
+import 'react-native-reanimated';
 import '../global.css';
 import { I18nProvider } from '../contexts/I18nContext';
 import { AuthProvider } from '../contexts/AuthContext';
